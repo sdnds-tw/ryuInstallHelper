1b38c4024ec0367754a0a9a29aed3eed3eeb8645
Che-Wei Lin <linton.tw@gmail.com>
2015-02-15 00:20:36 +0800

    Remove root permission when installation
    - When clone ryu source code the root permission is not required.
    - Change the git:// protocol to the https:// , because some organization or
      company will block the git protocol which using port (9418)

--- ryuInstallHelper.sh
+++ ryuInstallHelper.sh
@@ -2,43 +2,42 @@
 # This is a quick install script for ryu and dependencies.
 
 # Check Permission
-test $(id -u) -ne 0 && echo "This script must be run as root" && exit 0
+# test $(id -u) -ne 0 && echo "This script must be run as root" && exit 0
 
 # On Ubuntu 12.04 LTS
-# Do NOT use 512MB VM to compile ryu source code since there is not enough memory to compile source code 
+# Do NOT use 512MB VM to compile ryu source code since there is not enough memory to compile source code
 # Use SI standard, if you need more info, please try "man units"
 total_memory=$(free -b | grep 'Mem' | awk '{print $2}')
 minimum_memory=512000
 test $total_memory -lt $minimum_memory && echo "Minimum memory requirement is 512MB" && exit 0
 
 # Get the latest package lists
-apt-get update
+sudo apt-get update
 
 echo "##############################################################################"
 echo "#                          RYU DEPENDENCY CHECK                              #"
 echo "##############################################################################"
-apt-get install git python-pip build-essential python-dev libxml2-dev libxslt1-dev python-paramiko python-webob python-routes python-eventlet -y
+sudo apt-get install git python-pip build-essential python-dev libxml2-dev libxslt1-dev python-paramiko python-webob python-routes python-eventlet -y
 
 # Upgrade pip
-pip install -U pip six 
+sudo pip install -U pip six
 
 # install setuptools
-wget https://bootstrap.pypa.io/ez_setup.py -O - | python
+wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python
+sudo rm -rf setuptools-*.zip
 
 echo "##############################################################################"
 echo "#                         DOWNLOADING RYU FROM GIT                           #"
 echo "##############################################################################"
 
-cd $HOME
-git clone --depth 1 git://github.com/osrg/ryu.git
+git clone --depth 1 https://github.com/osrg/ryu.git
 
 echo "##############################################################################"
 echo "#                              BUILDING RYU                                  #"
 echo "##############################################################################"
 
-cd ryu; python ./setup.py install
+cd ryu; sudo python ./setup.py install
 
 echo "##############################################################################"
 echo "#                                    DONE                                    #"
 echo "##############################################################################"
-
