3954130c03826325823b8a50716e8f273e83d427
Yi Tseng <a86487817@gmail.com>
2016-05-19 23:41:23 +0800

    Improve installation script.

--- ryuInstallHelper.sh
+++ ryuInstallHelper.sh
@@ -4,7 +4,7 @@
 # Check Permission
 # test $(id -u) -ne 0 && echo "This script must be run as root" && exit 0
 
-# On Ubuntu 12.04 LTS
+# On Ubuntu 14.04 LTS
 # Do NOT use 512MB VM to compile ryu source code since there is not enough memory to compile source code
 # Use SI standard, if you need more info, please try "man units"
 total_memory=$(free -b | grep 'Mem' | awk '{print $2}')
@@ -14,23 +14,21 @@ test $total_memory -lt $minimum_memory && echo "Minimum memory requirement is 51
 # Get the latest package lists
 sudo apt-get update
 
+
 echo "##############################################################################"
 echo "#                          RYU DEPENDENCY CHECK                              #"
 echo "##############################################################################"
-sudo apt-get install git python-pip build-essential python-dev libxml2-dev libxslt1-dev python-paramiko python-webob python-routes wget -y
-
-# Upgrade pip
-sudo pip install -U pip six oslo.config==3.0.0 eventlet msgpack-python
-
-# install setuptools
-wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python
-sudo rm -rf setuptools-*.zip
+sudo apt-get install -y --force-yes git python-pip python3-pip build-essential python-dev python3-dev
 
 echo "##############################################################################"
 echo "#                         DOWNLOADING RYU FROM GIT                           #"
 echo "##############################################################################"
 
 git clone --depth 1 https://github.com/osrg/ryu.git
+# Upgrade pip
+sudo pip install pip six --upgrade
+
+sudo pip install -r ryu/tools/pip-requires
 
 echo "##############################################################################"
 echo "#                              BUILDING RYU                                  #"
@@ -42,3 +40,4 @@ echo "##########################################################################
 echo "#                         Try to use ./start_ryu.sh                          #"
 echo "#                                    DONE                                    #"
 echo "##############################################################################"
+
